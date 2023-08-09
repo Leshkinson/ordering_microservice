169a3a240006cb39d19de5ddf98ed9f794cb1bc1
Oleg Lopatko <vfY2minSRcF9ym7pT>
2023-08-09 17:18:21 +0300

    added auth and rebase auth

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,11 @@
-import { Module } from '@nestjs/common';
-import { AuthController } from './auth.controller';
+import cookieParser from 'cookie-parser';
 import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 
-@Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
-})
-export class AuthModule {}
+@Module({})
+export class AuthModule implements NestModule {
+  configure(consumer: MiddlewareConsumer): any {
+    consumer.apply(cookieParser()).forRoutes('*');
+  }
+}
